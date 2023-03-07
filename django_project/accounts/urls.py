033e048600cb43ce3760916f05afcdd67dfe1dae
Archlock20 <archlock20@gmail.com>
2023-03-07 13:18:51 -0500

    Add - Deteccion por imagen, Seccion "Utilidades"

--- django_project/accounts/urls.py
+++ django_project/accounts/urls.py
@@ -5,6 +5,7 @@ from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    #path("deteccion/", mask_detection_view.as_view(), name="mask_detection_view"),
     path('live_mask_detection/', views.live_mask_detection, name='live_mask_detection'),
+    path('deteccion/', views.live_mask_detection_view, name='deteccion'),
+    path('deteccion_img/', views.img_mask_detection, name='img_mask_detection'),
 ]
