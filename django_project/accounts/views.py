df5e2a2db18a6ed080d74d00ebc393437917f470
Archlock20 <archlock20@gmail.com>
2023-03-25 19:57:49 -0500

    Fix: Formato de correo de notificacion, Add: Archivos fuente de entrenamiento

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -19,13 +19,10 @@ from django.views.decorators.csrf import csrf_exempt
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from django.conf import settings
 from django.core.mail import send_mail
 from django.http import request
 from django.contrib.auth.models import User
-
-
-from django.contrib.auth.decorators import login_required
+import math
 
 
 class SignUpView(generic.CreateView):
@@ -33,9 +30,6 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
     
-    
-# Importar el modelo de reconocimiento de mascarillas
-model = tf.keras.models.load_model('static/modelo_cnn_deteccion_mascarillas_AG_MT_V3')
 
 
 """ def detect_mask(img):
@@ -88,9 +82,11 @@ model = tf.keras.models.load_model('static/modelo_cnn_deteccion_mascarillas_AG_M
                 cv2.putText(image,label_text,pt1,cv2.FONT_HERSHEY_SIMPLEX,0.5,color,2)
                 
         return image """
-        
+    
+# Importar el modelo de reconocimiento de mascarillas
+model = tf.keras.models.load_model('static/modelo_cnn_deteccion_mascarillas_AG_MT_V3')
+
  
-        
 def detect_mask(img, request):
         
         mask_count = 0
@@ -115,6 +111,13 @@ def detect_mask(img, request):
         #
         face_detection_model.setInput(blob)
         detection = face_detection_model.forward()
+        
+        # Paso 2 - Calibración de la cámara
+        focal_length = 500  # Focal length of the camera in pixels
+        known_distance = 1.0  # Known distance between two points in the real world in meters
+        known_width = 0.15  # Known width of the object being detected in meters
+        perceived_width = 0  # Perceived width of the object being detected in pixels
+        
         for i in range(0,detection.shape[2]):
             confidence = detection[0,0,i,2]
             if confidence > 0.5:
@@ -122,7 +125,8 @@ def detect_mask(img, request):
                 box = box.astype(int)
                 pt1 = (box[0],box[1])
                 pt2 = (box[2],box[3])
-                cv2.rectangle(image,pt1,pt2,(0,255,0),1)    
+                cv2.rectangle(image,pt1,pt2,(0,255,0),1)
+                
                 # Paso 2 - Preprosesamiento de los datos
                 face = image[box[1]:box[3],box[0]:box[2]]
                 face_blob = cv2.dnn.blobFromImage(face,1,(100,100),(104,117,123),swapRB=True)
@@ -138,24 +142,34 @@ def detect_mask(img, request):
                 confidence_index = result.argmax()
                 confidence_score = result[confidence_index]
                 label = etiquetas[confidence_index]
-                label_text = '{}: {:,.0f}%'.format(label,confidence_score*100)
+                label_text = '{}'.format(label)
                 #print(label_text)
                 color = getcolor(label)
                 cv2.rectangle(image,pt1,pt2,color,2)
                 cv2.putText(image,label_text,pt1,cv2.FONT_HERSHEY_SIMPLEX,0.5,color,2)
                 
+                # Paso 6 - Calcular la distancia en metros
+                perceived_width = box[2] - box[0]
+                distance_in_pixels = known_distance * focal_length / perceived_width
+                distance_in_meters = distance_in_pixels * known_width / perceived_width
+                print(f"Distancia: {distance_in_meters:.2f} metros")
+                
                 if label == 'Con Mascarilla':
                     mask_count += 1
                 else:
                     no_mask_count +=1
+                    cv2.putText(image, '---ATENCION---', (pt1[0], pt2[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
                     if no_mask_count > 3:
                         # Obtener la dirección de correo electrónico del usuario logueado
                         user_email = request.user.email
+                        user_name = request.user.username
                         
                         # Crear el mensaje de correo electrónico
-                        message = f"Subject: Alerta de deteccion de personas sin mascarilla\n\n\
-                            Se han detectado {no_mask_count} personas sin mascarilla en el sistema."
+                        """ message = f"Subject: Alerta de deteccion de personas sin mascarilla\n\nSe han detectado {no_mask_count} personas sin mascarilla en el sistema." """
                         
+                        message = f"Subject: Alerta de deteccion de personas sin mascarilla\n\nEstimado/a {user_name},\n\nLe informamos que en el sistema de monitoreo se han detectado {no_mask_count} personas sin mascarilla.\nEs importante tomar medidas inmediatas para garantizar la seguridad de todos los empleados y visitantes en las instalaciones.\nPor favor, realice una revision exhaustiva de las camaras en las areas mencionadas y asegurese de que se esten cumpliendo los protocolos de seguridad adecuados.\nSi tiene alguna pregunta o necesita ayuda adicional, no dude en contactarnos.\n\nAtentamente,\nFMask Detector System"
+
+
                         # Configurar la conexión al servidor SMTP y enviar el mensaje
                         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
                             smtp.ehlo()
